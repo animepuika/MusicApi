6b02e159a979cd0c2308110ccef2bf5633352acb
Kristers <gruz0001@hz.nl>
2024-06-24 16:17:25 +0200

    m

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,14 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MusixmatchController;
+use Illuminate\Support\Facades\Route;
 
+// Home route to display the search form
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+// API route to handle Musixmatch search requests
 Route::post('/api/musixmatch/search', [MusixmatchController::class, 'search']);
 
 
